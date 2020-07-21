054f2961fe4093c458ebbf8739e2e702824aa9d2
Hanz Montano <montanohanz@gmail.com>
2020-07-21 17:30:21 +0800

    Day 1

---
+++ day1.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+#print
+
+#Double quotes
+print("Hello world!")
+#Single Quotes
+print('Hello other world')
+#Special Characters in strings
+print(".*!/()")
+#Double Quote inside Single Quote
+print('"')
+print('Hello I am "cool". Hehe.')
+#Using \ as an escape characer
+print("Hello I am \"cool\". Hehe.")
+#String combinations
+print("Hello", "I am", "cool")
+
+#Math
+#Basic Math
+print(1+2)
+print(2-3)
+print(3*2/7)
+#PEMDAS
+print((3*2)/2*(4-2))
+#Exponents
+print(4**2)
+#Comparisons
+print(2<1)
+print(4>3)
+print(5>=5)
+print((3*2)/2*(4-2) == 6)
+#Modulo / Remainder
+print(5%2)
+print(27%8)
+
+#Variables
+i = 2 #Integer
+j = "Two" #String
+k = 3*2-4*1 #Math operation
+l= 4<5 #Comparison
+
+a = 2
+print(a)
+b = 2+2
+print(b)
+c = a+b
+print(c)
+d = "Two"
+print(d, a)
+
+cars = 100
+seats_in_a_car = 4
+total_people = cars*seats_in_a_car
+print("Total number of people:", total_people)
