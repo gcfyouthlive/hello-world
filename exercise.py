db3dda1ad272ef037d1d47402a14cc0e26a5a91a
Hanz Montano <montanohanz@gmail.com>
2020-07-21 17:08:55 +0800

    Initial Commit

---
+++ exercise.py
@@ -0,0 +1,13 @@
+#print
+
+#Double quotes
+print("Hello world!")
+#Single Quotes
+print('Hello other world')
+#Special Characters in strings
+print(".*!/()")
+#Double Quote inside Single Quote
+print('"')
+print('Hello I am "cool". Hehe.')
+#Using \ as an escape characer
+print("Hello I am \"cool\". Hehe.")
